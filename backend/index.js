6938ccb3b0f9c6c8dc141ff28e62e0f7d4c77e28
Dinesh <itsdinesh036@gmail.com>
2026-06-21 13:14:01 +0530

    enable the express to trust the proxy

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.set('trust proxy', 1);
 
 // Read the origins from the environment variable and split them into an array
 // Fallback to localhost if the variable is missing (for local dev safety)
